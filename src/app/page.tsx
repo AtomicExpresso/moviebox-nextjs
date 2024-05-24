034052a034f8e8c7e8286021965a34d2843cf636
atomic <baileyweb04@gmail.com>
2024-05-24 14:11:55 -0400

    removed verbose code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,54 @@
 'use client'
 
-import fetchData from '@/lib/api';
+import {fetchData, fetchDataGenreaAction} from '@/lib/api';
 import { useEffect, useState } from 'react';
 import {dataType} from '@/typeings/types';
 import rating from '@/assets/images/Rating.svg';
 import Image from 'next/image';
 
-export default function Home() {
-  const [data, setData] = useState<dataType[]>([])
-  
-  useEffect(() => {
-    const fetchAsyncData = async () => {
-      const fetchedData = await fetchData(14);
-        setData(fetchedData.results);
+const fetchDataFromAPI = async (url: string, queryParams: string = '') => {
+  try {
+    const response = await fetch(`${url}${queryParams}`, { method: 'GET', headers: { accept: 'application/json' } });
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-    fetchAsyncData();
-  }, [])
+export default function Home() {
+  const [data, setData] = useState<dataType[]>([]);
+  const [dataGenreAction, setDataGenreAction] = useState<dataType[]>([]);
+  const [dataNewMovies, setDataNewMovies] = useState<dataType[]>([]);
 
-  const CreateItems = () => {
-    return data.map((item, i) => {
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        const popularMovies = await fetchDataFromAPI('https://api.themoviedb.org/3/movie/popular', '?api_key=5864127d28cedcf6e5a23ad38b9d9816');
+        setData(popularMovies.results);
+
+        const actionMovies = await fetchDataFromAPI('https://api.themoviedb.org/3/discover/movie', `?api_key=5864127d28cedcf6e5a23ad38b9d9816&with_genres=28`);
+        setDataGenreAction(actionMovies.results);
+
+        const newMovies = await fetchDataFromAPI('https://api.themoviedb.org/3/movie/upcoming', `?api_key=5864127d28cedcf6e5a23ad38b9d9816`);
+        setDataNewMovies(newMovies.results);
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDataAsync();
+  }, []);
+  
+  //Genreates a list of movies from the state arrays
+  const CreateNewItems = ({mapTitle}: {mapTitle: dataType[]}) => {
+    return mapTitle.map((item) => {
     return (
       <div className='item-movie' key={item.id}>
         <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
@@ -29,20 +58,31 @@ export default function Home() {
           <Image draggable='false' src={rating} alt='rating'></Image>
           <h2>{item.vote_average}</h2>
           <div className='divider'></div>
-          {/* <h3>{item.genres.map(item => item.name)}</h3> */}
         </div>
     </div>
     </div>
     )
   })}
 
+  const CreateMovieCat = ({name, creation}: {name: string, creation: React.ReactNode}) => {
+    return (
+      <>
+        <div className='item-movie-cat-title'>
+          <h1>{name}</h1>
+          <hr></hr>
+        </div>
+        <div className='item-movie-holder'>
+          {creation}
+        </div>
+      </>
+    )
+  }
+
   return (
     <div className='discover-page'>
-      <h1>Popular</h1>
-      <hr></hr>
-      <div className='item-movie-holder'>
-        <CreateItems/>
-      </div>
+      <CreateMovieCat name='Popular' creation={<CreateNewItems mapTitle={data}/>}/>
+      <CreateMovieCat name='New' creation={<CreateNewItems mapTitle={dataNewMovies}/>}/>
+      <CreateMovieCat name='Action' creation={<CreateNewItems mapTitle={dataGenreAction}/>}/>
     </div>
   );
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,4 @@
+//Movie API
 const options = { method: 'GET', headers: { accept: 'application/json' } };
 
 //Fetchs data from movie api
@@ -14,4 +15,30 @@ const fetchData = (id: number) => {
     });
 };
 
-export default fetchData;
+const fetchDataGenreaAction = (id: number) => {
+  return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=5864127d28cedcf6e5a23ad38b9d9816&with_genres=28`, options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data; // Return the data to be used elsewhere if needed
+    })
+    .catch(error => {
+      console.error(error);
+      throw error; // Rethrow the error for error handling elsewhere if needed
+    });
+};
+
+const fetchDataNewMovie = (id: number) => {
+  return fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=5864127d28cedcf6e5a23ad38b9d9816`, options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data; // Return the data to be used elsewhere if needed
+    })
+    .catch(error => {
+      console.error(error);
+      throw error; // Rethrow the error for error handling elsewhere if needed
+    });
+};
+
+export {fetchData, fetchDataGenreaAction, fetchDataNewMovie};
